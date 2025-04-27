bac4f6deb7a1acc01aaec09dbc8c6e3ae087a686
Daniel Alfonsetti <daniel.alfonsetti@gmail.com>
2025-04-27 12:30:16 -0400

    No folding packages into single symlink

--- setup.sh
+++ setup.sh
@@ -5,13 +5,19 @@ stow_overwrite_replace() {
     # https://unix.stackexchange.com/questions/680413/opposite-of-adopt-option-for-gnu-stow
     echo "Will overwrite (with confirmation) any conflicting files with the version of the files from the last commit in this dotfile repo if they are conflicts."
     git stash # stash so that we only overwrite files with versions from the last commit of this repo.
+
+    # MAKE AND CHECKOUT TEMPORARY BRANCH AND SAVE NAME OF CURRENT BRANCH
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
+    temp_branch="stow_temp_$(date +%s)"
+    git checkout -b "$temp_branch" > /dev/null # Create and switch to temp branch
+
     stow --adopt */ --target ~ # find what files would change.
     detected_conflicts=$(git status --porcelain | grep '^ M' | awk '{print $2}')
     if [[ -z "$detected_conflicts" ]]; then
         echo "No conflicts detected."
     else
         for file in $detected_conflicts; do
-            read -p "Do you want to restore $file to the last committed version? (y/n): " restore_choice
+            read -p "Do you want to replace $file with the last committed version? (y/n): " restore_choice
             if [[ "$restore_choice" == "y" || "$restore_choice" == "Y" ]]; then
                 git checkout -- "$file"
             fi
@@ -19,13 +25,16 @@ stow_overwrite_replace() {
         git reset --hard 
         stow */ --target ~
     fi
+    # GO BACK TO ORIGINAL BRANCH AND DELETE TEMPO BRANCH
+    git checkout "$current_branch" > /dev/null
+    git branch -D "$temp_branch" > /dev/null
+
     git stash pop
 }
 
 
 PARENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 pushd $PARENT_DIR 
-echo "Current directory: $(pwd)"
 git submodule init
 git submodule update
 read -p "Do you want stow to try to overwrite your existing files? We will ask for confirmation for each individual conflict. (y/n): " choice
@@ -35,5 +44,4 @@ if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
 else
     stow */ --target ~
 fi
-
 popd
