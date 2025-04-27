70955c2935f7f423b7b2139191cd93a87431aa0b
Daniel Alfonsetti <daniel.alfonsetti@gmail.com>
2025-04-27 11:33:14 -0400

    Changed setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-cd "$(dirname "$0")" || exit
+
+PARENT_DIR="$(dirname "$0")"
+pushd $PARENT_DIR 
+echo "Current directory: $(pwd)"
 git submodule init
 git submodule update
 stow */ --target ~
+popd
