62bf8a4755dc06363b612bc2766d07a237784941
Daniel Alfonsetti <daniel.alfonsetti@gmail.com>
2025-06-19 14:10:39 -0400

    More updates

--- bash/bash_utils/.my_bash_functions.sh
+++ bash/bash_utils/.my_bash_functions.sh
@@ -2,3 +2,16 @@ ppath() {
     # n.b. tr stands for 'TRanslate'
     tr ':' '\n' <<< "$PATH"
 }
+
+clip() {
+    if command -v xclip >/dev/null 2>&1; then
+        xclip -selection clipboard
+    else
+        echo "Error: xclip is not installed." >&2
+        return 1
+    fi
+}
+
+rebash() {
+    source ~/.bashrc
+}
